109885c26b8609167382aa83a463de1d00ac91c0
SRINIVASA MANIKANTH <manikawnth@gmail.com>
2015-02-07 17:17:14 -0500

    adding angular

---
+++ client/controllers/app.js
@@ -0,0 +1,11 @@
+/**
+var new app = * newapp Modul;
+*
+* Description
+*/
+var new app = angular.module('newapp', []);
+
+newapp.controller('loginController', ['$scope,$http', function($scope,$http){
+	
+
+}])
